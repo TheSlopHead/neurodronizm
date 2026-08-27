19ed5f6ff25429c067471f50c051f331c7a77233
energon715 <TheSlopHead@outlook.com>
2026-08-27 23:12:04 +0300

    realized collector function added savepost func

--- cmd/collector/collector.go
+++ cmd/collector/collector.go
@@ -1,12 +1,15 @@
-package main
+package collector
 
 import (
+	"context"
 	"log"
+	"neurodronizm/cmd/store"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func main() {
+func Collector(ctx context.Context, s *store.Store) {
 	bot, err := tgbotapi.NewBotAPI("8979894456:AAHRe72wplOs-ylpQoer3S1MRLouov4RYj0")
 
 	if err != nil {
@@ -24,13 +27,23 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil {
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		if update.ChannelPost != nil {
+			post := update.ChannelPost
+			err := s.SavePost(ctx, int64(post.MessageID), post.Text, time.Unix(int64(post.Date), 0))
+			if err != nil {
+				log.Printf("failed to save post: %v", err)
+			} else {
+				log.Printf("Channel post %d saved succesfully", post.MessageID)
+			}
+			if update.Message != nil {
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+				msg.ReplyToMessageID = update.Message.MessageID
 
-			bot.Send(msg)
+				bot.Send(msg)
+			}
+
 		}
 	}
 }
