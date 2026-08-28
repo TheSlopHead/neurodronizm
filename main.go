d0b8f7cf90034535a0129d460b52e3899aaab547
energon715 <TheSlopHead@outlook.com>
2026-08-28 18:42:30 +0300

    realized .env file load, hided bot_token and db connection url

--- main.go
+++ main.go
@@ -5,11 +5,21 @@ import (
 	"log"
 	"neurodronizm/cmd/collector"
 	"neurodronizm/cmd/store"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+	
+	database_url := os.Getenv("DATABASE_URL")
 	ctx := context.Background()
-	s, err := store.New("postgres://dronism:change_me@localhost:5432/dronism?sslmode=disable")
+	s, err := store.New(database_url)
 	if err != nil {
 		log.Fatalf("failed to connect to postgres: %v", err)
 	}
