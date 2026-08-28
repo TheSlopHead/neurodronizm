8f9854a3695476caec1943595a4acd8b674f1e77
energon715 <TheSlopHead@outlook.com>
2026-08-28 18:40:39 +0300

    added image error handling

--- cmd/collector/collector.go
+++ cmd/collector/collector.go
@@ -4,13 +4,16 @@ import (
 	"context"
 	"log"
 	"neurodronizm/cmd/store"
+	"os"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func Collector(ctx context.Context, s *store.Store) {
-	bot, err := tgbotapi.NewBotAPI("8979894456:AAHRe72wplOs-ylpQoer3S1MRLouov4RYj0")
+
+	bot_token := os.Getenv("COLLECTOR_BOT_TOKEN")
+	bot, err := tgbotapi.NewBotAPI(bot_token)
 
 	if err != nil {
 		panic(err)
@@ -29,12 +32,21 @@ func Collector(ctx context.Context, s *store.Store) {
 	for update := range updates {
 		if update.ChannelPost != nil {
 			post := update.ChannelPost
-			err := s.SavePost(ctx, int64(post.MessageID), post.Text, time.Unix(int64(post.Date), 0))
-			if err != nil {
-				log.Printf("failed to save post: %v", err)
-			} else {
-				log.Printf("Channel post %d saved succesfully", post.MessageID)
+			if post.Text == "" && post.Caption == "" {
+				continue
 			}
+			if post.Text == "" && post.Caption != "" {
+				s.SavePost(ctx, int64(post.MessageID), post.Caption, time.Unix(int64(post.Date), 0))
+				//TODO - сделать нормально по условиям, создать переменную  для текста и обработать все возможные ошибки
+			} else {
+				err := s.SavePost(ctx, int64(post.MessageID), post.Text, time.Unix(int64(post.Date), 0))
+				if err != nil {
+					log.Printf("failed to save post: %v", err)
+				} else {
+					log.Printf("Channel post %d saved succesfully", post.MessageID)
+				}
+			}
+
 			if update.Message != nil {
 				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
