79179266d81a89c6a75de54c6d167a16fecf067b
energon715 <TheSlopHead@outlook.com>
2026-08-29 02:52:37 +0300

    style:add blank lines

--- main.go
+++ main.go
@@ -11,15 +11,19 @@ import (
 )
 
 func main() {
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
+
 	database_url := os.Getenv("DATABASE_URL")
 	ctx := context.Background()
+
 	s, err := store.New(database_url)
 	if err != nil {
 		log.Fatalf("failed to connect to postgres: %v", err)
 	}
+	
 	collector.Collector(ctx, s)
 }
