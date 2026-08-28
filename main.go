9c47c878d118d350a984a8fbd13182caeb58bc4e
energon715 <TheSlopHead@outlook.com>
2026-08-29 02:50:32 +0300

    style: add blank lines for readability

--- main.go
+++ main.go
@@ -3,20 +3,18 @@ package main
 import (
 	"context"
 	"log"
-	"neurodronizm/cmd/collector"
-	"neurodronizm/cmd/store"
+	"neurodronizm/internal/collector"
+	"neurodronizm/internal/store"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
-	
 	database_url := os.Getenv("DATABASE_URL")
 	ctx := context.Background()
 	s, err := store.New(database_url)
