d7f600a29bb937d4d190ff3e315211a42b53c5db
energon715 <TheSlopHead@outlook.com>
2026-09-04 21:48:56 +0300

    refactor: make logic for openai SDK

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -5,40 +5,59 @@ import (
 	"fmt"
 	"strings"
 
-	"google.golang.org/genai"
+	"github.com/openai/openai-go/v3"
+	"github.com/openai/openai-go/v3/option"
 )
 
 type Generator struct {
-	genaiClient *genai.Client
-	apiKey      string
+	openaiClient *openai.Client
+	apiKey       string
 }
 
 func New(ctx context.Context, apiKey string) (*Generator, error) {
-	client, err := genai.NewClient(ctx, &genai.ClientConfig{
-		APIKey:  apiKey,
-		Backend: genai.BackendGeminiAPI,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("Cannot create gemini client: %v", err)
-	}
+	client := openai.NewClient(
+		option.WithAPIKey("ollama"),
+		option.WithBaseURL("http://localhost:11434/v1/"),
+	)
 	return &Generator{
-		genaiClient: client,
-		apiKey:      apiKey,
+		openaiClient: &client,
+		apiKey:       " ",
 	}, nil
+
 }
+
+// func New(ctx context.Context, apiKey string) (*Generator, error) {
+// 	client, err := genai.NewClient(ctx, &genai.ClientConfig{
+// 		APIKey:  apiKey,
+// 		Backend: genai.BackendGeminiAPI,
+// 	})
+// 	if err != nil {
+// 		return nil, fmt.Errorf("Cannot create gemini client: %v", err)
+// 	}
+// 	return &Generator{
+// 		genaiClient: client,
+// 		apiKey:      apiKey,
+// 	}, nil
+// }
 
 func (g *Generator) GeneratePost(ctx context.Context, examples []string, topic string) ([]string, error) {
 	Prompt := strings.Join(examples, "\n---\n")
 	finalPrompt := fmt.Sprintf("Вот примеры моих постов:\n%s\n\nА теперь: %s", Prompt, topic)
 
-	parts := []*genai.Part{
-		{Text: finalPrompt},
-	}
-	rawResult, err := g.genaiClient.Models.GenerateContent(ctx, "gemini-3.1-flash-lite", []*genai.Content{{Parts: parts}}, nil)
+	rawResult, err := g.openaiClient.Chat.Completions.New(ctx, openai.ChatCompletionNewParams{
+		Model: openai.ChatModel("llama3.2:3b"),
+		Messages: ([]openai.ChatCompletionMessageParamUnion{
+			openai.UserMessage(finalPrompt),
+		}),
+	})
 	if err != nil {
 		return nil, fmt.Errorf("Cannot generate post: %v", err)
 	}
-	res := rawResult.Text()
+
+	if len(rawResult.Choices) == 0 {
+		return nil, fmt.Errorf("Ollama returned empty choices")
+	}
+	res := rawResult.Choices[0].Message.Content
 	variants := strings.Split(res, "[POST_SPLIT]")
 	cleanVariants := make([]string, 0, len(variants))
 
@@ -50,3 +69,36 @@ func (g *Generator) GeneratePost(ctx context.Context, examples []string, topic s
 	}
 	return cleanVariants, nil
 }
+
+func (f *Generator) GetEmbedding(ctx context.Context, text string) ([]float32, error) {
+	openai.EmbeddingNewParams{
+		Model: openai.ChatModel("nomic-embed-text"),
+		Input: (openai.EmbeddingNewParamsInputUnion{
+			OfString: openai.String(text),
+		}),
+	}
+}
+
+// func (g *Generator) GeneratePost(ctx context.Context, examples []string, topic string) ([]string, error) {
+// 	Prompt := strings.Join(examples, "\n---\n")
+// 	finalPrompt := fmt.Sprintf("Вот примеры моих постов:\n%s\n\nА теперь: %s", Prompt, topic)
+
+// 	parts := []*genai.Part{
+// 		{Text: finalPrompt},
+// 	}
+// 	rawResult, err := g.genaiClient.Models.GenerateContent(ctx, "gemini-3.1-flash-lite", []*genai.Content{{Parts: parts}}, nil)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("Cannot generate post: %v", err)
+// 	}
+// 	res := rawResult.Text()
+// 	variants := strings.Split(res, "[POST_SPLIT]")
+// 	cleanVariants := make([]string, 0, len(variants))
+
+// 	for _, variant := range variants {
+// 		trimmed := strings.TrimSpace(variant)
+// 		if trimmed != "" {
+// 			cleanVariants = append(cleanVariants, trimmed)
+// 		}
+// 	}
+// 	return cleanVariants, nil
+// }
