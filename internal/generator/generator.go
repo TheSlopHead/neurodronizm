f68f5de13f34059d004f1e2e633374f539cee654
energon715 <TheSlopHead@outlook.com>
2026-09-07 21:47:33 +0300

    chore: delete gemini api SDK

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -9,49 +9,6 @@ import (
 	"github.com/openai/openai-go/v3/option"
 )
 
-// type Generator struct {
-// 	genaiClient *genai.Client
-// 	apiKey      string
-// }
-
-// func New(ctx context.Context, apiKey string) (*Generator, error) {
-// 	client, err := genai.NewClient(ctx, &genai.ClientConfig{
-// 		APIKey:  apiKey,
-// 		Backend: genai.BackendGeminiAPI,
-// 	})
-// 	if err != nil {
-// 		return nil, fmt.Errorf("Cannot create gemini client: %v", err)
-// 	}
-// 	return &Generator{
-// 		genaiClient: client,
-// 		apiKey:      apiKey,
-// 	}, nil
-// }
-
-// func (g *Generator) GeneratePost(ctx context.Context, examples []string, topic string) ([]string, error) {
-// 	Prompt := strings.Join(examples, "\n---\n")
-// 	finalPrompt := fmt.Sprintf("Вот примеры моих постов:\n%s\n\nА теперь: %s", Prompt, topic)
-
-// 	parts := []*genai.Part{
-// 		{Text: finalPrompt},
-// 	}
-// 	rawResult, err := g.genaiClient.Models.GenerateContent(ctx, "gemini-3.1-flash-lite", []*genai.Content{{Parts: parts}}, nil)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("Cannot generate post: %v", err)
-// 	}
-// 	res := rawResult.Text()
-// 	variants := strings.Split(res, "[POST_SPLIT]")
-// 	cleanVariants := make([]string, 0, len(variants))
-
-// 	for _, variant := range variants {
-// 		trimmed := strings.TrimSpace(variant)
-// 		if trimmed != "" {
-// 			cleanVariants = append(cleanVariants, trimmed)
-// 		}
-// 	}
-// 	return cleanVariants, nil
-// }
-
 type Generator struct {
 	openaiClient *openai.Client
 	apiKey       string
